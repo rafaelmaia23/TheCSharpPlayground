597e6cd3b95fda9dedc8297d46c06ad9d902c6b4
Rafael Maia <rafaelmfcontato@protonmail.com>
2024-06-27 12:47:16 -0300

    Questões aleatórias de testes técnicos

---
+++ TestesTecnicos/AnaliseRelatorio.cs
@@ -0,0 +1,33 @@
+﻿namespace TheCSharpPlayground.TestesTecnicos;
+
+public class AnaliseRelatorio
+{
+    Relatorio AnalisaFaturamento(decimal[] faturamentosDiarios)
+    {
+        var faturamentosFiltrados = faturamentosDiarios.Where(valor => valor > 0);
+
+        Relatorio relatorio = new Relatorio()
+        {
+            menorFaturamento = faturamentosFiltrados.Min(),
+            maiorFaturamento = faturamentosFiltrados.Max(),
+            diasAcimaDaMedia = faturamentosFiltrados.Count(valor => valor > faturamentosFiltrados.Average())
+        };
+
+        return relatorio;
+    }
+}
+
+public class Relatorio
+{
+    public decimal menorFaturamento { get; set; }
+    public decimal maiorFaturamento { get; set; }
+    public int diasAcimaDaMedia { get; set; }
+
+    public void EscreverRelatorio()
+    {
+        Console.WriteLine($"Menor valor de faturamento: {menorFaturamento}");
+        Console.WriteLine($"Maior valor de faturamento: {maiorFaturamento}");
+        Console.WriteLine($"Número de dias com faturamento acima da média: {diasAcimaDaMedia}");
+    }
+}
+
---
+++ TestesTecnicos/Matematica.cs
@@ -0,0 +1,25 @@
+﻿namespace TheCSharpPlayground.TestesTecnicos;
+
+public class Matematica
+{
+    public int Soma(int a, int b) => a + b;
+
+    public void SomaEProduto(int a, int b, out int soma, out int produto)
+    {
+        soma = a + b;
+        produto = a * b;
+    }
+
+    int CalculaSomaDDeImparesEntre100e200()
+    {
+        int soma = 0;
+        for (int i = 100; i <= 200; i++)
+        {
+            if (i % 2 != 0)
+            {
+                soma += i;
+            }
+        }
+        return soma;
+    }
+}
